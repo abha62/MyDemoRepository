718f05ece7b9ebec7b7ae34bea1f21727f0aa390
Aman Bhattad <abha62@delbahamas31178.sapient.com>
2016-08-30 12:03:57 +0530

    My Intial Commit

---
+++ MyHibernate/src/beans/EmployeeTable.java
@@ -0,0 +1,34 @@
+package beans;
+
+public class EmployeeTable {
+	private String id;
+	private String name;
+	private String desig;
+	private int salary;
+	
+	public void setDesig(String desig) {
+		this.desig = desig;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDesig() {
+		return desig;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	
+}
